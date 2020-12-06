a695dbc1756e4d81be096bfe1f0c65d316c4aeee
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2020-12-06 21:30:45 +0100

    restructured main, re-added first task

--- src/day1.rs
+++ src/day1.rs
@@ -1,12 +1,24 @@
-pub fn day1(input: &Vec<String>) -> i32 {
+use super::common::TaskOfDay;
+
+pub fn day1(input: &Vec<String>, part: TaskOfDay) -> i32 {
     let converted = input
         .iter()
         .map(|s| s.parse::<i32>().expect("could not parse string to int {}"));
-    let t = iproduct!(
-        iproduct!(converted.clone(), converted.clone()).filter(|&(i, j)| i + j <= 2020),
-        converted
-    )
-    .find(|&(t, k)| t.0 + t.1 + k == 2020)
-    .unwrap();
-    t.0 .0 * t.0 .1 * t.1
+    match part {
+        TaskOfDay::First => {
+            let t = iproduct!(converted.clone(), converted.clone())
+                .find(|(i, j)| i + j == 2020)
+                .unwrap();
+            t.0 * t.1
+        }
+        TaskOfDay::Second => {
+            let t = iproduct!(
+                iproduct!(converted.clone(), converted.clone()).filter(|&(i, j)| i + j <= 2020),
+                converted
+            )
+            .find(|&(t, k)| t.0 + t.1 + k == 2020)
+            .unwrap();
+            t.0 .0 * t.0 .1 * t.1
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 extern crate itertools;
 use std::fs;
 use std::time::Instant;
+mod common;
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 mod day5;
 mod day6;
-mod common;
 use common::TaskOfDay;
 
 fn read_file_with_blank_lines(path: &String) -> Vec<String> {
@@ -18,94 +18,67 @@ fn read_file_with_blank_lines(path: &String) -> Vec<String> {
         .map(|s| s.trim())
         .map(|s| s.to_string())
         .collect();
-    res    
+    res
 }
 
 fn read_file(path: &String) -> Vec<String> {
     let lines = read_file_with_blank_lines(&path);
-    let res: Vec<String> = lines.iter()
+    let res: Vec<String> = lines
+        .iter()
         .filter(|s| s.len() > 0)
         .map(|s| s.clone())
         .collect();
     res
 }
 
-fn run_on_content<T>(f: fn(&Vec<String>) -> T, contents: &Vec<String>) -> T {
+fn run_on_content<T>(f: fn(&Vec<String>, TaskOfDay) -> T, contents: &Vec<String>) -> (T, T) {
     let now = Instant::now();
-    let res = f(contents);
+    let res_first = f(contents, TaskOfDay::First);
     let elapsed = now.elapsed();
-    println!("Elapsed: {:.2} millis", elapsed.as_millis());
-    res
+    println!("Elapsed first:\t{:.2} millis", elapsed.as_millis());
+    let now = Instant::now();
+    let res_second = f(contents, TaskOfDay::Second);
+    let elapsed = now.elapsed();
+    println!("Elapsed second:\t{:.2} millis", elapsed.as_millis());
+    (res_first, res_second)
 }
 
-fn run<T>(day: i32, f: fn(&Vec<String>) -> T) -> T {
-    let path = format!("res/input_{:02}.txt", day);
+fn get_path(day: i32) -> String {
+    println!("Running day {}", day);
+    format!("res/input_{:02}.txt", day)    
+}
+
+fn run<T>(day: i32, f: fn(&Vec<String>, TaskOfDay) -> T) -> (T, T) {
+    let path = get_path(day);
     let contents: Vec<String> = read_file(&path);
     run_on_content(f, &contents)
 }
 
-fn run_with_blank_lines<T>(day: i32, f: fn(&Vec<String>) -> T) -> T {
-    let path = format!("res/input_{:02}.txt", day);
+fn run_with_blank_lines<T>(day: i32, f: fn(&Vec<String>, TaskOfDay) -> T) -> (T, T) {
+    let path = get_path(day);
     let contents: Vec<String> = read_file_with_blank_lines(&path);
     run_on_content(f, &contents)
 }
 
-fn main() {
-    println!("day1 {}", run(1, day1::day1));
-    println!(
-        "day2, first {}",
-        run(2, |input| day2::day2(input, TaskOfDay::First))
-    );
-    println!(
-        "day2, second {}",
-        run(2, |input| day2::day2(input, TaskOfDay::Second))
-    );
-    println!(
-        "day3, first {}",
-        run(3, |input| day3::day3(input, TaskOfDay::First))
-    );
-    println!(
-        "day3, second {}",
-        run(3, |input| day3::day3(input, TaskOfDay::Second))
-    );
-    println!(
-        "day4, first {}",
-        run_with_blank_lines(4, |input| day4::day4(input, TaskOfDay::First))
-    );
-    println!(
-        "day4, second {}",
-        run_with_blank_lines(4, |input| day4::day4(input, TaskOfDay::Second))
-    );
-    println!(
-        "day5, first {}",
-        run(5, |input| day5::day5(input, TaskOfDay::First))
-    );
-    println!(
-        "day5, second {}",
-        run(5, |input| day5::day5(input, TaskOfDay::Second))
-    );
-    println!(
-        "day6, first {}",
-        run_with_blank_lines(6, |input| day6::day6(input, TaskOfDay::First))
-    );
-    println!(
-        "day6, second {}",
-        run_with_blank_lines(6, |input| day6::day6(input, TaskOfDay::Second))
-    );
+fn print_res<T: std::fmt::Display>(res: (T, T)) {
+    println!("{}, {}\n", res.0, res.1);
+}
 
+fn main() {
+    print_res(run(1, day1::day1));
+    print_res(run(2, day2::day2));
+    print_res(run(3, day3::day3));
+    print_res(run_with_blank_lines(4, day4::day4));
+    print_res(run(5, day5::day5));
+    print_res(run_with_blank_lines(6, day6::day6));
 }
 
 #[test]
-fn test(){
-    assert_eq!(run(1, day1::day1), 48914340);
-    assert_eq!(run(2, |input| day2::day2(input, TaskOfDay::First)), 434);
-    assert_eq!(run(2, |input| day2::day2(input, TaskOfDay::Second)), 509);
-    assert_eq!(run(3, |input| day3::day3(input, TaskOfDay::First)), 247);
-    assert_eq!(run(3, |input| day3::day3(input, TaskOfDay::Second)), 2983070376i64);
-    assert_eq!(run_with_blank_lines(4, |input| day4::day4(input, TaskOfDay::First)), 247);
-    assert_eq!(run_with_blank_lines(4, |input| day4::day4(input, TaskOfDay::Second)), 145);
-    assert_eq!(run(5, |input| day5::day5(input, TaskOfDay::First)), 938);
-    assert_eq!(run(5, |input| day5::day5(input, TaskOfDay::Second)), 696);
-    assert_eq!(run_with_blank_lines(6, |input| day6::day6(input, TaskOfDay::First)), 6542);
-    assert_eq!(run_with_blank_lines(6, |input| day6::day6(input, TaskOfDay::Second)), 3299);
+fn test() {
+    assert_eq!(run(1, day1::day1), (1007331, 48914340));
+    assert_eq!(run(2, day2::day2), (434, 509));
+    assert_eq!(run(3, day3::day3), (247, 2983070376i64));
+    assert_eq!(run_with_blank_lines(4, day4::day4), (247, 145));
+    assert_eq!(run(5, day5::day5), (938, 696));
+    assert_eq!(run_with_blank_lines(6, day6::day6), (6542, 3299));
 }
