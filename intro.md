e6ae36469ea680944eaadaba588bdcfcae502e5e
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2020-12-07 08:49:03 +0100

    piping results to readme with Python

---
+++ intro.md
@@ -0,0 +1,10 @@
+# Advent of Code 2020
+First steps in Rust. I use 
+VS Code with the extensions 
+- CodeLLDB 
+- Rust Test Lens
+- rust-analyzer 
+
+on  Windows 10.
+
+# Current Results
---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+
+
+def main():
+    tmp_file = "tmp_res.txt"
+    os.system(f"cargo run --release > {tmp_file}")
+    with open("intro.md", "r") as f:
+        intro = f.read()
+    with open(tmp_file, "r") as f:
+        res = f.read()
+    with open("README.md", "w") as f:
+        f.write(f"{intro}\n{res}\n")
+
+
+if __name__ == "__main__":
+    main()
