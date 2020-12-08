b2a452e6797634188612c8b7af8c09874ceacb8a
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2020-12-08 12:07:46 +0100

    removed unnecessary temp-file

--- run.py
+++ run.py
@@ -1,15 +1,13 @@
 import os
+import subprocess
 
 
 def main():
-    tmp_file = "tmp_res.txt"
-    os.system(f"cargo run --release > {tmp_file}")
     with open("intro.md", "r") as f:
         intro = f.read()
-    with open(tmp_file, "r") as f:
-        res = f.read()
+    cargo_out = subprocess.check_output("cargo run --release").decode("utf-8") 
     with open("README.md", "w") as f:
-        f.write(f"{intro}\n```\n{res}```\n")
+        f.write(f"{intro}\n```\n{cargo_out}```\n")
 
 
 if __name__ == "__main__":
