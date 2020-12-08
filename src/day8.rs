409291293abff192dc0788827696af8325e700c4
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2020-12-08 10:00:38 +0100

    str to enum

--- src/day8.rs
+++ src/day8.rs
@@ -2,49 +2,59 @@ use super::common::split_in2_tuple;
 use super::common::TaskOfDay;
 use std::collections::HashSet;
 
-fn map_instructions(input: &Vec<String>) -> Vec<(&str, i32)> {
+enum Op {
+    Jmp,
+    Nop,
+    Acc,
+}
+
+fn str_2_op(s: &str) -> Op {
+    match s {
+        "jmp" => Op::Jmp,
+        "nop" => Op::Nop,
+        "acc" => Op::Acc,
+        _ => panic!("Unknown op {}", s),
+    }
+}
+
+fn map_instructions(input: &Vec<String>) -> Vec<(Op, i32)> {
     input
         .iter()
         .map(|s| {
-            let (instruction, num_str) = split_in2_tuple(s, " ");
-            (instruction, num_str.parse::<i32>().unwrap())
+            let (operation, num_str) = split_in2_tuple(s, " ");
+            (str_2_op(operation), num_str.parse::<i32>().unwrap())
         })
-        .collect::<Vec<(&str, i32)>>()
+        .collect::<Vec<(Op, i32)>>()
 }
 
-fn accumulator_at_loop_or_termination(instructions: &Vec<(&str, i32)>) -> (i32, bool) {
+fn accumulator_at_loop_or_termination(operations: &Vec<(Op, i32)>) -> (i32, bool) {
     let mut set: HashSet<usize> = HashSet::with_capacity(0);
     let mut accumulator = 0i32;
     let mut idx = 0usize;
-    while !set.contains(&idx) && idx < instructions.len() {
+    while !set.contains(&idx) && idx < operations.len() {
         set.insert(idx);
-        match instructions[idx].0 {
-            "acc" => {
-                accumulator += instructions[idx].1;
+        match operations[idx].0 {
+            Op::Acc => {
+                accumulator += operations[idx].1;
                 idx += 1usize;
             }
-            "nop" => {
+            Op::Nop => {
                 idx += 1usize;
             }
-            "jmp" => {
-                let tmp = idx as i32 + instructions[idx].1;
+            Op::Jmp => {
+                let tmp = idx as i32 + operations[idx].1;
                 idx = tmp as usize;
-            }
-            _ => {
-                panic!("Unknown instruction {}.", instructions[idx].0);
             }
         }
     }
-    (accumulator, idx < instructions.len())
+    (accumulator, idx < operations.len())
 }
 
-fn swap(ins: &str) -> &str {
-    if ins == "nop" {
-        "jmp"
-    } else if ins == "jmp" {
-        "nop"
-    } else {
-        panic!("AAAAAAAAAHH!");
+fn swap(op: &Op) -> Op {
+    match op {
+        Op::Jmp => Op::Nop,
+        Op::Nop => Op::Jmp,
+        Op::Acc => panic!("Acc is not swapable."),
     }
 }
 
@@ -57,38 +67,49 @@ pub fn run(input: &Vec<String>, part: TaskOfDay) -> i32 {
             let swap_candidates = instructions
                 .iter()
                 .enumerate()
-                .filter(|(_, (s, _))| s == &"nop" || s == &"jmp")
-                .map(|(i, _)| i).collect::<Vec<usize>>();
+                .filter(|(_, (op, _))| match op {
+                    Op::Jmp => true,
+                    Op::Nop => true,
+                    Op::Acc => false,
+                })
+                .map(|(i, _)| i)
+                .collect::<Vec<usize>>();
 
             for i in swap_candidates {
-                instructions[i].0 = swap(instructions[i].0);
+                instructions[i].0 = swap(&instructions[i].0);
 
                 let (acc, looping) = accumulator_at_loop_or_termination(&instructions);
                 if looping {
                     // undo swap and try next
-                    instructions[i].0 = swap(instructions[i].0);   
-                }
-                else {
+                    instructions[i].0 = swap(&instructions[i].0);
+                } else {
                     return acc;
                 }
             }
-            panic!("Could not repair instructions.");
+            panic!("Could not repair operations.");
         }
     }
 }
 
 #[test]
 fn test() {
+    fn op_to_num(op: &Op) -> i32 {
+        match op {
+            Op::Acc => 0i32,
+            Op::Nop => 1i32,
+            Op::Jmp => 2i32,
+        }
+    }
     let ref_instructions = vec![
-        ("nop", 0i32),
-        ("acc", 1i32),
-        ("jmp", 4i32),
-        ("acc", 3i32),
-        ("jmp", -3i32),
-        ("acc", -99i32),
-        ("acc", 1i32),
-        ("jmp", -4i32),
-        ("acc", 6i32),
+        (Op::Nop, 0i32),
+        (Op::Acc, 1i32),
+        (Op::Jmp, 4i32),
+        (Op::Acc, 3i32),
+        (Op::Jmp, -3i32),
+        (Op::Acc, -99i32),
+        (Op::Acc, 1i32),
+        (Op::Jmp, -4i32),
+        (Op::Acc, 6i32),
     ];
 
     let input_str = "nop +0
@@ -102,7 +123,7 @@ jmp -4
 acc +6";
     let input: Vec<String> = input_str.split("\n").map(|s| s.to_string()).collect();
     for (ref_ins, ins) in izip!(ref_instructions.iter(), map_instructions(&input).iter()) {
-        assert_eq!(ref_ins.0, ins.0);
+        assert_eq!(op_to_num(&ref_ins.0), op_to_num(&ins.0));
         assert_eq!(ref_ins.1, ins.1);
     }
     assert_eq!(run(&input, TaskOfDay::First), 5);
