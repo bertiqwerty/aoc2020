fd381b6d4c34b7c27657b2834918e3aeafd55d78
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2020-12-08 12:14:36 +0100

    gathered strings at top

--- run.py
+++ run.py
@@ -3,10 +3,14 @@ import subprocess
 
 
 def main():
-    with open("intro.md", "r") as f:
+    intro_file = "intro.md"
+    readme_file = "README.md"
+    cargo_cmd = "cargo run --release"
+    
+    with open(intro_file, "r") as f:
         intro = f.read()
-    cargo_out = subprocess.check_output("cargo run --release").decode("utf-8") 
-    with open("README.md", "w") as f:
+    cargo_out = subprocess.check_output(cargo_cmd).decode("utf-8") 
+    with open(readme_file, "w") as f:
         f.write(f"{intro}\n```\n{cargo_out}```\n")
 
 
